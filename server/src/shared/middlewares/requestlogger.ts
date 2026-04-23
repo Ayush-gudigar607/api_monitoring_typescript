23f3cd8d728ca6064a868e54b0d400f0779239b4
unknown <23ai009@sitmng.ac.in>
2026-04-23 23:07:21 +0530

    add the requestlogger.ts

---
+++ server/src/shared/middlewares/requestlogger.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import { logger } from "../config/logger";
+
+const requestLogger = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  const start = Date.now();
+
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+
+    logger.info(
+      `${req.method} ${req.originalUrl} ${res.statusCode} - ${duration}ms`,
+      {
+        method: req.method,
+        url: req.originalUrl,
+        statusCode: res.statusCode,
+        duration,
+      }
+    );
+  });
+
+  next();
+};
+
+export default requestLogger;
